e885a320fb2236f3e2c09058595697f7151fbdb7
Sergey Vorobyev <syvorobyev@gmail.com>
2025-06-06 12:29:48 +0200

    deploy

--- src/expandables/script.js
+++ src/expandables/script.js
@@ -1,6 +1,14 @@
 export const useExpandables = () => {
     const expandables = document.querySelectorAll('.expandable');
 
+    const intersectionObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.target.isExpanded && !entry.isIntersecting) {
+                entry.target.collapse();
+            }
+        });
+    }, { rootMargin: '-25% 0% -25% 0%' });
+
     expandables.forEach((expandable) => {
         const container = expandable.querySelector('.expandable__container');
         const openBtn = expandable.querySelector('[data-trigger="show-more"]');
@@ -15,12 +23,18 @@ export const useExpandables = () => {
         openBtn.addEventListener('click', expand);
         closeBtn.addEventListener('click', collapse);
 
+        intersectionObserver.observe(expandable);
+
+        expandable.isExpanded = true;
+        expandable.collapse = collapse;
+
         function expand() {
             gsap.timeline()
                 .to(container, { display: 'block' })
                 .to(openBtn, { display: 'none' })
                 .to(closeBtn, { display: 'block' })
-                .to(container, { height: 'auto', duration: 0.4});
+                .to(container, { height: 'auto', duration: 0.4})
+                .add(() => expandable.isExpanded = true);
         }
 
         function collapse() {
@@ -28,7 +42,8 @@ export const useExpandables = () => {
                 .to(closeBtn, { display: 'none' })
                 .to(openBtn, { display: 'block' })
                 .to(container, { height: 0, duration: 0.4 })
-                .to(container, { display: 'none' });
+                .to(container, { display: 'none' })
+                .add(() => expandable.isExpanded = false);
         }
     });
 };
